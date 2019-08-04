4668ac4f7006b3839b871a3c8b1ea5412586cae6
Vinh T. Nguyen <vinh.nguyen@ttu.edu>
2019-08-04 16:28:13 -0500

    update

--- components/robobehavior.js
+++ components/robobehavior.js
@@ -1,16 +1,16 @@
 AFRAME.registerComponent('behavior', {
     init:function () {
-        this.fightTimes = 10;
+        this.fightTimes = 5;
         let self = this;
        this.el.addEventListener('movingstarted',function () {
            self._walking();
-       })
+       });
         this.el.addEventListener('movingended',function () {
             self._punch();
-        })
+        });
     },
     _walking:function(){
-        this.el.setAttribute('animation-mixer',{clip:'Walking'})
+        this.el.setAttribute('animation-mixer',{clip:'mixamo.com'})
     },
     _idle:function(){
         this.el.setAttribute('animation-mixer',{clip:'Idle'})
@@ -18,19 +18,40 @@ AFRAME.registerComponent('behavior', {
     },
     _punch:function(){
         let time = this.fightTimes;
-        let target = document.querySelector("#target");
+        let target1 = document.querySelector("#target1");
         let self = this;
         this.el.setAttribute('animation-mixer',{clip:'Punch'});
-        target.setAttribute('animation-mixer',{clip:'Punch'});
+        target1.setAttribute('animation-mixer',{clip:'Punch'});
         let countdown = setInterval(function () {
             time--;
             if(time <=0){
-                target.setAttribute('animation-mixer',{clip:'Running'});
-                target.setAttribute('alongpath', {curve: '#track2', dur: 5000, rotate: true});
-                target.addEventListener('movingended',function () {
-                    target.setAttribute('animation-mixer',{clip:'Idle'});
+
+                target1.setAttribute('animation-mixer',{clip:'mixamo.com'});
+                target1.setAttribute('alongpath', {curve: '#track2', dur: 5000, rotate: true});
+                target1.addEventListener('movingended',function () {
+                    target1.removeAttribute('animation-mixer');
                 });
-                self.el.setAttribute('animation-mixer',{clip:'Idle'});
+                let target2 = document.querySelector("#target2");
+                let target3 = document.querySelector("#target3");
+
+                target2.setAttribute('animation-mixer',{clip:'mixamo.com'});
+                target2.setAttribute('alongpath', {curve: '#track3', dur: 5000})
+
+                target2.addEventListener('movingended',function () {
+                    target2.removeAttribute('animation-mixer');
+                });
+
+                target3.setAttribute('animation-mixer',{clip:'mixamo.com'});
+                target3.setAttribute('alongpath', {curve: '#track4', dur: 5000})
+
+                target3.addEventListener('movingended',function () {
+                    target3.removeAttribute('animation-mixer');
+                });
+
+                // self.el.setAttribute('alongpath', {curve: '#smove2', dur: 5000});
+                //                 // self.el.addEventListener('movingended',function () {
+                //                 //     self.el.setAttribute('animation-mixer',{clip:''});
+                //                 // });
                 clearInterval(countdown);
             }
         },1000)
