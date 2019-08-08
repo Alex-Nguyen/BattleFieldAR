ba1ba005265ebb292ed6b9f2804365ac747f2ee8
Vinh T. Nguyen <vinh.nguyen@ttu.edu>
2019-08-08 01:11:22 -0500

    update

--- components/scenemanager.js
+++ components/scenemanager.js
@@ -2,13 +2,12 @@
 AFRAME.registerComponent('manager', {
     init: function () {
         let self = this;
+
         this.directionV3 = new THREE.Vector3();
         this.stage1Animation=false;
         this.stage1Handler = this.stage1Handler.bind(this);
         $( "#stage1" ).click(self.stage1Handler);
-        let clipPlane = new THREE.Plane().setFromNormalAndCoplanarPoint(
-            new THREE.Vector3(0,1,0), new THREE.Vector3(0,0,0) );
-        this.el.sceneEl.renderer.clippingPlanes = [clipPlane];
+
         let pos1 = document.querySelector('#pos1').object3D.visible;
         let pos2 = document.querySelector('#pos2').object3D.visible;
         let pos3 = document.querySelector('#pos3').object3D.visible;
@@ -27,7 +26,38 @@ AFRAME.registerComponent('manager', {
     },
     stage1Handler:function(){
         this.stage1Animation=true;
+        var el = document.createElement('a-entity');
+        el.setAttribute('gltf-model','#man');
+        let target = document.querySelector('#union')
+        target.parentNode.removeChild(target);
 
+        let scene = document.querySelector('a-scene');
+        let pos1 = document.querySelector('#pos1').object3D;
+        let pos2 =document.querySelector('#pos2').object3D.position;
+
+        el.object3D.position.x = pos1.position.x;
+        el.object3D.position.y = pos1.position.y;
+        el.object3D.position.z = pos1.position.z;
+        scene.appendChild(el);
+
+        createjs.Tween.get(el.object3D.position).to(pos2, 3000)
+        // target.parentNode.removeChild(target);
+        // let direction = this.directionV3;
+        // let targetPos = pos2.getWorldPosition();
+        // let currPos = pos1.getWorldPosition();
+        // direction.copy(targetPos).sub(currPos);
+        // let distance = direction.length();
+        // if(distance < 0.03){return;}
+        // let factor = 0.0001/distance;
+        // ['x','y','z'].forEach(axis=>{
+        //     direction[axis] *= factor*(timeDelta/1000);
+        // });
+        // target.setAttribute('position',{
+        //     x:currPos.x+direction.x,
+        //     y:currPos.y+direction.y,
+        //
+        //
+        // })
 
         },
     stage2Handler:function(){
@@ -74,33 +104,14 @@ AFRAME.registerComponent('manager', {
     tick:function (time, timeDelta) {
         let pos1 = document.querySelector('#pos1').object3D;
         let pos2 = document.querySelector('#pos2').object3D;
-        let pos3 = document.querySelector('#pos3').object3D;
-        let pos4 = document.querySelector('#pos4').object3D;
-        let pos5 = document.querySelector('#pos5').object3D;
-        let pos6 = document.querySelector('#pos6').object3D;
+        // let pos3 = document.querySelector('#pos3').object3D;
+        // let pos4 = document.querySelector('#pos4').object3D;
+        // let pos5 = document.querySelector('#pos5').object3D;
+        // let pos6 = document.querySelector('#pos6').object3D;
         let flag1 = pos1.visible === true && pos2.visible === true;
+
         $('#stage1').prop('disabled', !flag1);
-        if(this.stage1Animation){
-            let target = document.querySelector('#union');
 
-            let direction = this.directionV3;
-            let targetPos = pos2.getWorldPosition();
-            let currPos = pos1.getWorldPosition();
-            direction.copy(targetPos).sub(currPos);
-            let distance = direction.length();
-            if(distance < 0.03){return;}
-            let factor = 0.0001/distance;
-            ['x','y','z'].forEach(axis=>{
-                direction[axis] *= factor*(timeDelta/1000);
-            });
-            target.setAttribute('position',{
-                x:currPos.x+direction.x,
-                y:currPos.y+direction.y,
-
-
-            })
-
-        }
 
 
     }
