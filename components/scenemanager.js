5c730dccb2a179f3f6666f419e21af9710f6e708
Vinh T. Nguyen <vinh.nguyen@ttu.edu>
2019-08-08 01:28:52 -0500

    update

--- components/scenemanager.js
+++ components/scenemanager.js
@@ -8,24 +8,11 @@ AFRAME.registerComponent('manager', {
         this.stage1Handler = this.stage1Handler.bind(this);
         $( "#stage1" ).click(self.stage1Handler);
 
-        let pos1 = document.querySelector('#pos1').object3D.visible;
-        let pos2 = document.querySelector('#pos2').object3D.visible;
-        let pos3 = document.querySelector('#pos3').object3D.visible;
-        let pos4 = document.querySelector('#pos4').object3D.visible;
-        let pos5 = document.querySelector('#pos5').object3D.visible;
-        let pos6 = document.querySelector('#pos6').object3D.visible;
 
-        // this.stage2Handler = this.stage2Handler.bind(this);
-        // this.stage3Handler = this.stage3Handler.bind(this);
-        // this.stage4Handler = this.stage4Handler.bind(this);
-        // this.el.addEventListener('StartStage1',this.stage1Handler);
-        // this.el.addEventListener('StartStage2',this.stage2Handler);
-        // this.el.addEventListener('StartStage3',this.stage3Handler);
-        // this.el.addEventListener('StartStage4',this.stage4Handler);
-        // this.el.emit('StartStage1','',false)
     },
     stage1Handler:function(){
         this.stage1Animation=true;
+        let self = this;
         var el = document.createElement('a-entity');
         el.setAttribute('gltf-model','#man');
         let target = document.querySelector('#union')
@@ -40,55 +27,44 @@ AFRAME.registerComponent('manager', {
         el.object3D.position.z = pos1.position.z;
         scene.appendChild(el);
 
-        createjs.Tween.get(el.object3D.position).to(pos2, 3000)
-        // target.parentNode.removeChild(target);
-        // let direction = this.directionV3;
-        // let targetPos = pos2.getWorldPosition();
-        // let currPos = pos1.getWorldPosition();
-        // direction.copy(targetPos).sub(currPos);
-        // let distance = direction.length();
-        // if(distance < 0.03){return;}
-        // let factor = 0.0001/distance;
-        // ['x','y','z'].forEach(axis=>{
-        //     direction[axis] *= factor*(timeDelta/1000);
-        // });
-        // target.setAttribute('position',{
-        //     x:currPos.x+direction.x,
-        //     y:currPos.y+direction.y,
-        //
-        //
-        // })
+        createjs.Tween.get(el.object3D.position).to(pos2, 3000).call(self.stage2Handler)
+
 
         },
     stage2Handler:function(){
-        console.log("Start stage 2")
-        let self = this;
-        let timer = 3;
-        let action = setInterval(function () {
-            timer--;
-            $("#countdown").text(`Scene 2 will start in ${timer} seconds`);
-            if(timer <=0){
-                let t1 = document.querySelector("#target1");
-                let t2 = document.querySelector("#target2");
-                let t3 = document.querySelector("#target3");
-                t1.setAttribute('animation-mixer',{clip:'mixamo.com'});
-                t1.setAttribute('alongpath', {curve: '#track2', dur: 5000, rotate: true});
-                t2.setAttribute('animation-mixer',{clip:'mixamo.com'});
-                t2.setAttribute('alongpath', {curve: '#track3', dur: 5000, rotate: true});
-                t3.setAttribute('animation-mixer',{clip:'mixamo.com'});
-                t3.setAttribute('alongpath', {curve: '#track4', dur: 5000, rotate: true});
-                t1.addEventListener('movingended',function () {
-                    t1.removeAttribute('animation-mixer');
-                    t2.removeAttribute('animation-mixer');
-                    t3.removeAttribute('animation-mixer');
-                    $("#countdown").text(`Stage 2 clear - Start Stage 3`);
-                    self.el.emit('StartStage3','',false);
-                });
+        let scene = document.querySelector('a-scene');
+        let pos2 =document.querySelector('#pos2').object3D.position;
+        let pos3 =document.querySelector('#pos3').object3D.position;
+        let pos4 =document.querySelector('#pos4').object3D.position;
+        let pos5 =document.querySelector('#pos5').object3D.position;
+        let el1 = document.createElement('a-entity');
+        el1.setAttribute('gltf-model','#man');
+        el1.object3D.position.x = pos2.position.x;
+        el1.object3D.position.y = pos2.position.y;
+        el1.object3D.position.z = pos2.position.z;
+        scene.appendChild(el1);
+
+        let el2 = document.createElement('a-entity');
+        el2.setAttribute('gltf-model','#man');
+        el2.object3D.position.x = pos2.position.x;
+        el2.object3D.position.y = pos2.position.y;
+        el2.object3D.position.z = pos2.position.z;
+        scene.appendChild(el2);
 
 
-                clearInterval(action)
-            }
-        },1000)
+        let el3 = document.createElement('a-entity');
+        el3.setAttribute('gltf-model','#man');
+        el3.object3D.position.x = pos2.position.x;
+        el3.object3D.position.y = pos2.position.y;
+        el3.object3D.position.z = pos2.position.z;
+        scene.appendChild(el3);
+
+        createjs.Tween.get(el1.object3D.position).to(pos3, 3000);
+        createjs.Tween.get(el2.object3D.position).to(pos4, 3000);
+        createjs.Tween.get(el3.object3D.position).to(pos5, 3000);
+
+
+
 
     },
     stage3Handler:function(){
